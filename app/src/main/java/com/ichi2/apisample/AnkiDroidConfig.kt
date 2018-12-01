34a44f13580b206b379d3da86ab81df0f158c00c
Anton Sheremet <antonysheremet@gmail.com>
2018-12-01 03:49:02 +0300

    minor code refactoring

--- app/src/main/java/com/ichi2/apisample/AnkiDroidConfig.kt
+++ app/src/main/java/com/ichi2/apisample/AnkiDroidConfig.kt
@@ -6,11 +6,11 @@ object AnkiDroidConfig {
     // Name of deck which will be created in AnkiDroid
     val DECK_NAME = "ImporterSample"
     // Name of model which will be created in AnkiDroid
-    internal const val MODEL_NAME = "com.ichi2.plaintextimporter"
+    const val MODEL_NAME = "com.ichi2.plaintextimporter"
     // Optional space separated list of tags to add to every note
-    internal const val TAGS = "AnkiImporter2"
+    const val TAGS = "AnkiImporter2"
     // List of field names that will be used in AnkiDroid model
-    internal val FIELDS = arrayOf("Front", "Back")
+    val FIELDS = arrayOf("Front", "Back")
 
     // Define two keys which will be used when using legacy ACTION_SEND intent
     val FRONT_SIDE_KEY = FIELDS[0]
@@ -22,16 +22,16 @@ object AnkiDroidConfig {
     </HashMap> */
     val exampleData: ArrayList<HashMap<String, String>>
         get() {
-            val EXAMPLE_FRONTS = arrayOf("front1", "front2")
-            val EXAMPLE_BACKS = arrayOf("back1", "back2")
+            val exampleFronts = arrayOf("front1", "front2")
+            val exampleBacks = arrayOf("back1", "back2")
 
-            val data = ArrayList<HashMap<String, String>>()
-            for (idx in EXAMPLE_FRONTS.indices) {
-                val hm = HashMap<String, String>()
-                hm[FIELDS[0]] = EXAMPLE_FRONTS[idx]
-                hm[FIELDS[1]] = EXAMPLE_BACKS[idx]
-                data.add(hm)
+            return ArrayList<HashMap<String, String>>().apply {
+                for (idx in exampleFronts.indices) {
+                    val hm = HashMap<String, String>()
+                    hm[FIELDS[0]] = exampleFronts[idx]
+                    hm[FIELDS[1]] = exampleBacks[idx]
+                    add(hm)
+                }
             }
-            return data
         }
 }
